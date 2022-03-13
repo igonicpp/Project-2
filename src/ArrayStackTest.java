63242aa7558dfa0bf7f093fcf59796b713711a75
Alex Mariano <amarianocello@gmail.com>
2022-03-13 15:00:07 -0700

    Added ArrayStackTest.java methods - Alex

--- src/ArrayStackTest.java
+++ src/ArrayStackTest.java
@@ -1,3 +1,74 @@
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.junit.Before;
+
 public class ArrayStackTest {
+
+
+    /**
+     * Several cases to test:
+     * Pushing on an empty stack
+     * Pushing an object
+     * Pushing a Null object (throws exception)
+     */
+    @Test
+    public void testPush(){
+
+    }
+
+    /**
+     * Several cases to test:
+     * Retrieving from an empty stack (throws exception)
+     * Retrieving the top of the stack successfully
+     * Retrieving the 2nd of the stack
+     */
+    @Test
+    public void testPop(){
+
+    }
+
+    /**
+     * Several cases to test:
+     * Peeking from an empty stack (throws exception)
+     * Peeking the top of the stack successfully
+     */
+    @Test
+    public void testPeek(){
+
+    }
+
+    /**
+     * Several cases to test:
+     * Stack is empty
+     * Stack is not empty
+     */
+    @Test
+    public void testIsEmpty(){
+
+    }
+
+    /**
+     * Several cases to test:
+     * Stack is empty
+     * Stack is not empty
+     */
+    @Test
+    public void testClear(){
+
+    }
+
+    /**
+     *  This tests the evaluation the postfix expression
+     */
+    @Test
+    public void testEvaluatePostFix(){
+
+    }
+
 
 }
