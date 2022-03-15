69171506d4fa0001fc2666eb7648033bc4bbc857
Alex Mariano <amarianocello@gmail.com>
2022-03-14 19:32:00 -0700

    Added TestCases - Alex

--- src/ArrayStackTest.java
+++ src/ArrayStackTest.java
@@ -7,18 +7,45 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.Before;
 
+import java.util.EmptyStackException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class ArrayStackTest {
 
+    StackInterface arrayStack = new ResizableArrayStack();
 
+    /**
+     * Initializes an ArrayStack to use.
+     */
+    @Before
+    public void setUp(){
+        testClear();
+    }
     /**
      * Several cases to test:
      * Pushing on an empty stack
      * Pushing an object
-     * Pushing a Null object (throws exception)
+     * Pushing a Null object
      */
     @Test
     public void testPush(){
-
+        try {
+            arrayStack.push("a");
+        } catch (Exception e){
+            fail("Failure: push() threw an exception: " + e);
+        }
+        try {
+            arrayStack.push("b");
+        } catch (Exception e){
+            fail("Failure: push() threw an exception: " + e);
+        }
+        try {
+            arrayStack.push(null);
+        } catch (Exception e){
+            fail("Failure: push() threw an exception: " + e);
+        }
     }
 
     /**
@@ -29,7 +56,16 @@ public class ArrayStackTest {
      */
     @Test
     public void testPop(){
-
+        try {
+            arrayStack.pop();
+            fail("Failure: Successfully pop into an empty bag.");
+        } catch (EmptyStackException e){
+            assertEquals(EmptyStackException.class, e.getClass());
+        }
+        arrayStack.push("1stEntry");
+        arrayStack.push("2ndEntry");
+        assertEquals("2ndEntry", arrayStack.pop());
+        assertEquals("1stEntry", arrayStack.pop());
     }
 
     /**
@@ -39,7 +75,14 @@ public class ArrayStackTest {
      */
     @Test
     public void testPeek(){
-
+        try {
+            arrayStack.peek();
+            fail("Failure: Successfully peeked into an empty bag.");
+        } catch (EmptyStackException e){
+            assertEquals(EmptyStackException.class, e.getClass());
+        }
+        arrayStack.push("a");
+        assertEquals("a", arrayStack.peek());
     }
 
     /**
@@ -49,7 +92,9 @@ public class ArrayStackTest {
      */
     @Test
     public void testIsEmpty(){
-
+        assertEquals(true, arrayStack.isEmpty());
+        arrayStack.push("a");
+        assertEquals(false, arrayStack.isEmpty());
     }
 
     /**
@@ -59,7 +104,11 @@ public class ArrayStackTest {
      */
     @Test
     public void testClear(){
-
+        arrayStack.clear();
+        assertEquals(true, arrayStack.isEmpty());
+        arrayStack.push("a");
+        arrayStack.clear();
+        assertEquals(true, arrayStack.isEmpty());
     }
 
     /**
