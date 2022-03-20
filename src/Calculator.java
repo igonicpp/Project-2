1fde4658b5ab9707b55278551bf01699af1cbc89
Koki <kyamaguchi@cpp.edu>
2022-03-19 22:36:04 -0700

    fixed checkCapacity()

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,14 +15,11 @@ public class Calculator {
     }
 
 
-    /**
-     * JUnit test calling upon methods in CalculatorTest.class. Prints out failures. If Successful, prints out successful = true message.
-     */
     public static void testCalculator(){
         Result result = JUnitCore.runClasses(CalculatorTest.class);
         for (Failure failure : result.getFailures()){
             System.out.println(failure.toString());
         }
-        System.out.println("Resizable Array Bag test cases were successful? = " + result.wasSuccessful());
+        System.out.println("Resizable Array Stack test cases were successful? = " + result.wasSuccessful());
     }
 }
